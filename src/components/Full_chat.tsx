65d3a38697096253b7923c70f672cfcb5ed9b494
menotifilho <menotimfilho@gmail.com>
2023-02-04 16:10:37 -0300

    px atualizado

--- src/components/Full_chat.tsx
+++ src/components/Full_chat.tsx
@@ -5,7 +5,7 @@ import Top from "./Top";
 
 function Full_chat() {
 	return (
-		<div className=" max-w-sm lg:max-w-5xl flex flex-col h-full">
+		<div className=" px-2 max-w-sm lg:max-w-5xl flex flex-col h-full">
 			<Top></Top>
 			<Chat></Chat>
 		</div>
